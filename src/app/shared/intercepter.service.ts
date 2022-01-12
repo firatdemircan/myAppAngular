9dc7cf2530395ac445dcb3950d6a1b2e8caaa85f
firatdemircan <demircanfirat@gmail.com>
2022-01-12 14:41:32 +0300

    asdf

--- src/app/shared/intercepter.service.ts
+++ src/app/shared/intercepter.service.ts
@@ -1,6 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse} from "@angular/common/http";
-import {map, Observable} from "rxjs";
+import {catchError, map, Observable, throwError} from "rxjs";
+import {AuthService} from "./guards/auth.service";
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
@@ -8,45 +10,44 @@ import {map, Observable} from "rxjs";
 export class IntercepterService implements HttpInterceptor {
   private nreq: HttpRequest<any>;
 
-  constructor() {
+  constructor(private authService :AuthService,private router : Router) {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-    console.log("sdklfj")
-    let token = sessionStorage.getItem("token")
-    if (token != undefined && token.length > 0) {
-      this.nreq = req.clone({
-        setHeaders: {
-          Authentication: "Bearer " + token,
-          'Content-Type': 'application/json',
-          Accept: 'application/json',
-        }
-      });
+    let nreq = req.clone({
+      setHeaders: {
+        Authentication: this.authService.authenticated
+          ? `Bearer ${this.authService.token}`
+          : '',
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    });
 
-    }
+    //set header token
 
-    //adding token to header ends here!!
-
-    return next.handle(this.nreq)
-      .pipe(map((x) => {
-
-
-        if (x instanceof HttpResponse) {
-          let httpres = x as HttpResponse<any>;
-          if (httpres.body) {
-            if (httpres.body.status == 500) {
-              //to login
+    return next.handle(nreq).pipe(
+      map((res) => {
+        if (res instanceof HttpResponse) {
+          if (res.body) {
+            if (res.body.status == 401) {
+              this.authService.logOut();
+              this.router.navigate(['']);
             }
-
           }
-        } else {
-
         }
 
-        return x;
-
-      }));
+        return res;
+      }),
+      catchError((e) => {
+        if (e.error.status == 401) {
+          this.authService.logOut();
+          this.router.navigate(['']);
+        }
+        return throwError(e);
+      })
+    );
 
 
   }
